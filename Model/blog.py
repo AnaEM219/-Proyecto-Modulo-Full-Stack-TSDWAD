813885f40d14672fbb754cdaf1c46e1a01a13be7
Florencia Morisse <florenciamorisse@gmail.com>
2022-11-09 13:09:49 -0300

    Update blog.py

--- Model/blog.py
+++ Model/blog.py
@@ -1,14 +1,25 @@
 import pymysql
 print("resultado exitoso")
 
-mybbdd= pymysql.connect(
-    host='localhost',
-    user='root',
-    passwd= "fsfal4ever",
-    db="mountainhikedef"
-)
-mycursor= mybbdd.cursor()
-mycursor.execute("SELECT fecha, titulo FROM blog")
-for fecha, titulo in mycursor.fetchall():
-    print (fecha, titulo)
-mybbdd.close()
+class Blog():
+    
+    def __init__(self)-> None:
+        try:
+            self.mybbdd= pymysql.connect(
+                host='localhost',
+                user='root',
+                passwd= "fsfal4ever",
+                db="mountainhikedef"
+                )
+        except pymysql.Error as descriptionerror:
+            print("No se conectó", descriptionerror)
+    def InsertarValor(self):
+        if self.mybbdd.conn.ping(True):
+            try:
+                mycursor= self.mybbdd.cursor()                 
+                mycursor.execute("SELECT fecha, titulo FROM blog")
+                for fecha, titulo in mycursor.fetchall():
+                    print (fecha, titulo)
+                self.mybbdd.close()
+            except ConnectionError:
+                print("No se pudo jsdfklj")
