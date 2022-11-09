2368f48c70edd9a71c8ad81f6f7bdaa73767f2e2
Florencia Morisse <florenciamorisse@gmail.com>
2022-11-09 13:48:35 -0300

    update y read del blog

--- Model/blog.py
+++ Model/blog.py
@@ -9,11 +9,21 @@ connection= pymysql.connect(
     charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor
 )
+
+#UPDATE
+# try:
+#     with connection.cursor() as cursor:
+#         sql = "UPDATE blog SET fecha= '1 de enero' WHERE id_blog = 1;"
+#         cursor.execute(sql)
+#         connection.commit()
+# finally:
+#     connection.close()
+#READ
 try:
     with connection.cursor() as cursor:
-        sql = "UPDATE blog SET fecha= '1 de enero' WHERE id_blog = 1;"
-        cursor.execute(sql)
-        connection.commit()
-    
+        sql = "SELECT fecha, titulo, cuerpo FROM blog WHERE id_blog = %s;"
+        cursor.execute(sql, ('2'))
+        result=cursor.fetchone()
+        print(result)
 finally:
     connection.close()
