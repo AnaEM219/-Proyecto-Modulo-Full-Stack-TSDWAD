9ae647847cf8e4f07666316f7725c53e8228b66e
Florencia Morisse <florenciamorisse@gmail.com>
2022-11-10 07:45:18 -0300

    terminé el CRUD

--- Model/blog.py
+++ Model/blog.py
@@ -11,13 +11,13 @@ connection= pymysql.connect(
 )
 
 #UPDATE
-# try:
-#     with connection.cursor() as cursor:
-#         sql = "UPDATE blog SET fecha= '1 de enero' WHERE id_blog = 1;"
-#         cursor.execute(sql)
-#         connection.commit()
-# finally:
-#     connection.close()
+try:
+    with connection.cursor() as cursor:
+        sql = "UPDATE blog SET fecha= '1 de enero' WHERE id_blog = 1;"
+        cursor.execute(sql)
+        connection.commit()
+finally:
+    connection.close()
 #READ
 try:
     with connection.cursor() as cursor:
@@ -25,5 +25,21 @@ try:
         cursor.execute(sql, ('2'))
         result=cursor.fetchone()
         print(result)
+finally:
+    connection.close()
+#CREATE
+try:
+    with connection.cursor() as cursor:
+        sql="INSERT INTO blog (admin_id_admin, fecha, titulo, cuerpo) VALUES (%s,%s,%s, %s);"
+        cursor.execute(sql, (1,'10 de Noviembre','Fuertes LLuvias se esperan para Diciembre', 'El servicio meteorológico prevee lluvias abundantes y poco comunes para esta época del año'))
+        connection.commit()
+finally:
+    connection.close()
+#UPDATE
+try:
+    with connection.cursor() as cursor:
+        sql="UPDATE blog SET fecha=%s, titulo=%s, cuerpo=%s WHERE id_blog=22;"
+        cursor.execute(sql, ('11 de Noviembre', 'november rain', 'El servicio meteorológico anuncia una gran tormente para mediados de este mes'))
+        connection.commit()
 finally:
     connection.close()
