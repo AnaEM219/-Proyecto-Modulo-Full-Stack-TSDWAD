de86120c6d888462346e26e9bc85e5eac2fec0bb
Florencia Morisse <florenciamorisse@gmail.com>
2022-11-10 09:24:47 -0300

    Update blog.py

--- Model/blog.py
+++ Model/blog.py
@@ -10,38 +10,40 @@ try:
         cursorclass=pymysql.cursors.DictCursor
     )
     print("conexión exitosa")
+
+#CREATE
+    try:
+        with connection.cursor() as cursor:
+            sql="INSERT INTO blog (admin_id_admin, fecha, titulo, cuerpo) VALUES (%s,%s,%s, %s);"
+            cursor.execute(sql, (1,'10 de Noviembre','Fuertes LLuvias se esperan para Diciembre', 'El servicio meteorológico prevee lluvias abundantes y poco comunes para esta época del año'))
+            connection.commit()
+    finally:
+        connection.close()
+#READ
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM blog WHERE id_blog = %s;"
+            cursor.execute(sql, ('2'))
+            result=cursor.fetchall()
+            print(result)
+    finally:
+        connection.close()
+#UPDATE
+    try:
+        with connection.cursor() as cursor:
+            sql="UPDATE blog SET fecha=%s, titulo=%s, cuerpo=%s WHERE id_blog=%s;"
+            cursor.execute(sql, ('11 de Noviembre', 'November rain', 'El servicio meteorológico anuncia una gran tormente para mediados de este mes',22))
+            connection.commit()
+    finally:
+        connection.close()
+#DELETE
+    try:
+        with connection.cursor() as cursor:
+            sql= "DELETE FROM blog WHERE id_blog=%s;"
+            cursor.execute(sql, 14)
+            connection.commit()
+    finally:
+        connection.close()
+        
 except (pymysql.err.OperationalError, pymysql.err.InternalError) as e:
     print("Ocurrió un error al conectar: ", e)
-#CREATE
-# try:
-#     with connection.cursor() as cursor:
-#         sql="INSERT INTO blog (admin_id_admin, fecha, titulo, cuerpo) VALUES (%s,%s,%s, %s);"
-#         cursor.execute(sql, (1,'10 de Noviembre','Fuertes LLuvias se esperan para Diciembre', 'El servicio meteorológico prevee lluvias abundantes y poco comunes para esta época del año'))
-#         connection.commit()
-# finally:
-#     connection.close()
-# #READ
-# try:
-#     with connection.cursor() as cursor:
-#         sql = "SELECT fecha, titulo, cuerpo FROM blog WHERE id_blog = %s;"
-#         cursor.execute(sql, ('2'))
-#         result=cursor.fetchone()
-#         print(result)
-# finally:
-#     connection.close()
-# #UPDATE
-# try:
-#     with connection.cursor() as cursor:
-#         sql="UPDATE blog SET fecha=%s, titulo=%s, cuerpo=%s WHERE id_blog=%s;"
-#         cursor.execute(sql, ('11 de Noviembre', 'November rain', 'El servicio meteorológico anuncia una gran tormente para mediados de este mes',22))
-#         connection.commit()
-# finally:
-#     connection.close()
-#DELETE
-# try:
-#     with connection.cursor() as cursor:
-#         sql= "DELETE FROM blog WHERE id_blog=%s;"
-#         cursor.execute(sql, 14)
-#         connection.commit()
-# finally:
-#     connection.close()
